a536fe06435e35b9ef116d023edcffb6dd7a8520
Artem Lipodat <a.lipodat@mail.ru>
2023-03-17 12:33:39 +0400

    spectrecss

--- gulpfile.js
+++ gulpfile.js
@@ -51,7 +51,7 @@ function scripts() {
 function styles() {
     return src('app/' + preprocessor + '/main.' + preprocessor + '') // Выбираем источник: "app/sass/main.sass" или "app/less/main.less"
         .pipe(eval(preprocessor)()) // Преобразуем значение переменной "preprocessor" в функцию
-        .pipe(concat('app.min.css')) // Конкатенируем в файл app.min.js
+        .pipe(concat('app.min.css')) // Конкатенируем в файл app.min.css
         .pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true })) // Создадим префиксы с помощью Autoprefixer
         .pipe(cleancss( { level: { 1: { specialComments: 0 } }/* , format: 'beautify' */ } )) // Минифицируем стили
         .pipe(dest('app/css/')) // Выгрузим результат в папку "app/css/"
